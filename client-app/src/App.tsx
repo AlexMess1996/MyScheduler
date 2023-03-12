727425ef1b085fe9d4ce31e3f191174b804ff9ee
Norvigos <alexredfield13@gmail.com>
2023-03-12 21:35:50 +0100

    Sematic UI React has been implemented.

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
+import { Header, List } from 'semantic-ui-react';
 
 function App() {
   const [activities, setActivities] = useState([]);
@@ -16,15 +17,17 @@ function App() {
   },[])  
   return (
     <div>
-      <ul>
+        <Header as='h2' icon='users' content='Reactivities'/>
+        <List>
           {activities.map((activity: any) => (
-            <li key={activity.id}>
+            <List.Item key={activity.id}>
               {activity.title}
-            </li>
+            </List.Item>
             
           ))}
-        </ul>
+        </List>
     </div>
+
   );
 }
 
