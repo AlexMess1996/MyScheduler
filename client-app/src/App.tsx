e1ed73d2d3c8af472e7c0bd7dadd6a95101e1e9a
Norvigos <alexredfield13@gmail.com>
2023-03-12 21:32:14 +0100

    Client page can fetch data from the API

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,24 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [activities, setActivities] = useState([]);
+
+  useEffect:
+     useEffect(()=>{
+    //get some data
+    axios.get('http://localhost:5100/api/activities')
+    .then(response =>{
+      setActivities(response.data); 
+    })
+  },[])  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>
+          {activities.map((activity: any) => (
+            <li key={activity.id}>
+              {activity.title}
+            </li>
+            
+          ))}
+        </ul>
     </div>
   );
 }
